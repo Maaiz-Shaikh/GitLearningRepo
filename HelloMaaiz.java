d7e4c21418efad1aa4564c255b294870121d9c9b
Maaiz <maaizshaikh555@gmail.com>
2021-07-26 21:24:48 +0530

    Initial Commit

---
+++ HelloMaaiz.java
@@ -0,0 +1,19 @@
+package introduction;
+
+
+
+public class HelloMaaiz {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Maaiz, Welcome to java coding journey");
+		
+		int variable = 5;
+		System.out.println("variable = " + variable);
+	     
+	     
+	     
+
+	}
+
+}
