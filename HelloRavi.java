70f8737ae84a7b8781fc25cdd5760aba3f169ee4
Ravipatel1309 <ravivpatel1309@gmail.com>
2022-08-25 18:58:52 +0530

    HelloRavi.java file added

---
+++ HelloRavi.java
@@ -0,0 +1,11 @@
+public class HelloRavi {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello I'm Ravi here.");
+
+        System.out.println("Successfully forked and commited.");
+
+    }
+
+}
